6ed2cbdf63bbcf5ad56f154a30659ed26b6dfa77
Gabriel Deleon <gabrieldeleon@gabriels-mbp.myfiosgateway.com>
2021-08-27 08:55:29 -0400

    menus and categories services completed

---
+++ client/src/services/categories.js
@@ -0,0 +1,38 @@
+import api from './api-config'
+
+export const getAllCategories = async () => {
+    try {
+    const res = await api.get('/categories')
+    return res.data
+    } catch(e) {
+        throw e;
+    }
+}
+
+
+export const addCategoryToMenu = async (menuId, categoryData) => {
+    try {
+    const res = await api.post(`menus/${menuId}/categories`, {category: categoryData})
+    return res.data
+    } catch(e) {
+        throw e;
+    }
+}
+
+export const putCategory = async(menuId,categoryId,categoryData) => {
+    try {
+    const res = await api.put(`menus/${menuId}/categories/${categoryId}` {category: categoryData})
+    return res.data
+    } catch(e) {
+        throw e;
+    }
+}
+
+export const deleteCategory = async(menuId, categoryId) => {
+    try {
+    const res = await api.delete(`menus/${menuId}/categories/${categoryId}`)
+    return res
+    } catch(e) {
+        throw e;
+    }
+}
